87d3607392514eb4f63217fb6bc95cc1161254c4
Puspak29 <samantapuspak@gmail.com>
2026-02-15 16:24:02 +0530

    fix: fix relode issue

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,23 +1,10 @@
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css'
 import { Header, Footer } from '../components'
-import { useEffect } from 'react';
 
 
 function Layout() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const navigationEntries = performance.getEntriesByType("navigation");
-    const isReload =
-      navigationEntries.length > 0 &&
-      (navigationEntries[0] as PerformanceNavigationTiming).type === "reload";
-
-    if (isReload) {
-      navigate("/", { replace: true });
-    }
-  }, []);
   return (
     <div>
       <Header />
